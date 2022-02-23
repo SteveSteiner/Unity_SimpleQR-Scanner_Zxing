d5f542067f4be2023635ee745c9cc816fecd77cd
SteveSteiner <stvstnr@googlemail.com>
2022-02-23 11:23:02 +0100

    Added Camera Functionality; changed Zxing from Repo to .DLL; Successful Build

--- Assets/Scripts/QrCodeScanner.cs
+++ Assets/Scripts/QrCodeScanner.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+using ZXing;
 public class QrCodeScanner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Fields
+    [SerializeField]
+    private RawImage rimg_bgImage;
+
+    [SerializeField]
+    private AspectRatioFitter aspectRatioFitter;
+
+    [SerializeField]
+    private TextMeshProUGUI txt_qrResult;
+
+    [SerializeField]
+    private RectTransform rt_Scanzone;
+
+    private bool isCamAvailable;
+    private WebCamTexture cameraTexture;
+    #endregion
+
+    #region Mono methods
     void Start()
     {
-        
+        SetupCamera();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    #endregion
+
+    #region Methods
+    private void SetupCamera()
+    {
+        WebCamDevice[] webCamDevices = WebCamTexture.devices;
+
+        if(webCamDevices.Length == 0)
+        {
+            isCamAvailable = false;
+            txt_qrResult.text = "No Camera Available";
+            Debug.LogWarning("No Camera Available");
+            return;
+        }
+
+        foreach(WebCamDevice wcd in webCamDevices)
+        {
+            if(!wcd.isFrontFacing)
+            {
+                cameraTexture = new WebCamTexture(wcd.name, (int)rt_Scanzone.rect.width, (int)rt_Scanzone.rect.height);
+            }
+        }
+
+        cameraTexture.Play();
+        rimg_bgImage.texture = cameraTexture;
+        isCamAvailable = true;
+    }
+    #endregion
 }
